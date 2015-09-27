efe40fcdd3698accae5a3e11d6f583bb81b187dd
nliinanki <trolldans@gmail.com>
2015-09-27 10:10:46 +0000

    lab 4.1 task 1 and 2 and basic structure for javascript

---
+++ site.js
@@ -0,0 +1,28 @@
+// hello world
+function function101() {
+    console.log("Hello World");
+}
+
+function function102() {
+    alert("Rad1" + '\n' + "Rad2");
+}
+
+// medeltal av 4 tal
+function function201() {
+    var average = function(tal1, tal2, tal3, tal4) {
+        return (tal1 + tal2 + tal3 + tal4) / 4;
+    };
+    console.log(average(2, 2, 3, 3));
+}
+
+function function202() {
+    var printOut = function(argumentOne, argumentTwo) {
+        console.log(argumentOne +", " +argumentTwo);
+    };
+    printOut("Utan", "fantasi");
+}
+
+
+
+
+// gissa talet
